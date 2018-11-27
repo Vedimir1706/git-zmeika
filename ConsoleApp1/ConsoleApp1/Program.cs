e038a6f975ee71fb6b49acb9cbada398652c937d
vedimir1706 <pro13pre@yandex.ru>
2018-11-27 20:15:35 +0600

    создание змейки завершено. далее движение

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(80, 25);
+            Console.SetBufferSize(80, 25);
+
+            //Отрисовка рамочки
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 0, 24, '+');
+            VerticalLine rightLine = new VerticalLine(78, 0, 24, '+');
+            upLine.Draw();
+            downLine.Draw();
+            leftLine.Draw();
+            rightLine.Draw();
+
+           // отрисовка точек
+            Point p2 = new Point(4, 5, '*');
+            Snake snake = new Snake(p2, 4, Direction.RIGHT);
+            snake.Draw();
+            Console.ReadLine();
+        }
+    }
+}
